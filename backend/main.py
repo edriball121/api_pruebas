5bb6d50adb419494ab21d8f51e2f3e49ca214743
edriball121 <edriball@gmail.com>
2026-05-13 14:26:09 -0500

    fix: Se ajusta cors.

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://api-pruebas-1-ak82.onrender.com",
+        "https://api-pruebas-y9uv.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
